764a6fc60a211374c0030e65a4ddcccfc4df620b
ekosykh <evgenii.kosykh@t-systems.com>
2018-12-09 00:50:34 +0300

    The skeleton Spring Boot 2 application.

---
+++ FencingFightApplication/src/main/java/vrn/edinorog/FencingFightApplication.java
@@ -0,0 +1,12 @@
+package vrn.edinorog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FencingFightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FencingFightApplication.class, args);
+    }
+}
