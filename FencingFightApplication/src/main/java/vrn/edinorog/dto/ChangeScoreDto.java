cf1507df9433b091f245b0304cf8110b74bb33a7
Evgenii Kosykh <evgenii.kosykh@t-systems.com>
2018-12-12 17:40:02 +0300

    Added lombok.

--- FencingFightApplication/src/main/java/vrn/edinorog/dto/ChangeScoreDto.java
+++ FencingFightApplication/src/main/java/vrn/edinorog/dto/ChangeScoreDto.java
@@ -1,41 +1,17 @@
 package vrn.edinorog.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class ChangeScoreDto {
 
     private String fighter;
     private String kind;
     private int newValue;
-
-    public ChangeScoreDto() {}
-
-    public ChangeScoreDto(String fighter, String kind, int newValue) {
-        this.fighter = fighter;
-        this.kind = kind;
-        this.newValue = newValue;
-    }
-
-    public String getFighter() {
-        return fighter;
-    }
-
-    public void setFighter(String fighter) {
-        this.fighter = fighter;
-    }
-
-    public String getKind() {
-        return kind;
-    }
-
-    public void setKind(String kind) {
-        this.kind = kind;
-    }
-
-    public int getNewValue() {
-        return newValue;
-    }
-
-    public void setNewValue(int newValue) {
-        this.newValue = newValue;
-    }
-
 }
